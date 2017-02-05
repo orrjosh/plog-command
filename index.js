32c296c06a814db31a2b6e3c613787d3160b3867
Josh Orr <jgorr@instructure.com>
2017-02-05 01:10:26 -0600

    Adding cli options and method scaffold

--- index.js
+++ index.js
@@ -4,8 +4,18 @@ var cli = require('commander')
 
 cli
   .arguments('<message>')
-  .option('-a')
+  .option('-a, --add', 'Add log entry')
+  .option('-t, --time <time>', 'Timestamp in military time')
   .action(function(message) {
     console.log('message:', message)
+    if(cli.add){ add(message, timestamp) }
   })
   .parse(process.argv)
+
+function add(message, time){
+  if(!time){
+    // set time to cur military time
+  }
+  // gather log dir from home dir .plog
+  // append to current days log, creating if necessary
+}
