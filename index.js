c937baf65dd78a90505c073df87046534e3eb889
Josh Orr <jgorr@instructure.com>
2017-02-05 01:21:22 -0600

    add config file and default

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 #!/usr/local/bin/node
 
 var cli = require('commander')
+var cfg = require('home-config').load('.plog', {
+    log_dir : '~/Documents/plog/'
+});
+cfg.save()
 
 cli
   .arguments('<message>')
   .option('-a, --add', 'Add log entry')
-  .option('-t, --time <time>', 'Timestamp in military time')
+  .option('-t, --timestamp <timestamp>', 'Timestamp in military time')
   .action(function(message) {
-    console.log('message:', message)
-    if(cli.add){ add(message, timestamp) }
+    if(cli.add){ add(message, cli.timestamp) }
   })
   .parse(process.argv)
 
 function add(message, time){
+  console.log('message:', message)
   if(!time){
     // set time to cur military time
   }
